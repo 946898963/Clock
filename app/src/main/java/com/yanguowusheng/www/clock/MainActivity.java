a93fb0205f9afefd9b5d1c811528f229261a3719
946898963 <yanguowusheng123@gmail.com>
2016-01-27 18:27:13 +0800

    shiyanyixia

--- app/src/main/java/com/yanguowusheng/www/clock/MainActivity.java
+++ app/src/main/java/com/yanguowusheng/www/clock/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new Clock(this));
+        //实验一下
     }
 }
