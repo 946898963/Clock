0aadd885338e9dad17ea685d67f93ad96de474a3
946898963 <yanguowusheng123@gmail.com>
2016-01-27 17:43:45 +0800

    initial commit

---
+++ app/src/main/java/com/yanguowusheng/www/clock/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yanguowusheng.www.clock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new Clock(this));
+    }
+}
